68f9f41a499a0b9d79a22721a1de387f03048b1c
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2026-04-04 15:30:33 +0200

    connect server to atlas database.

--- server.js
+++ server.js
@@ -3,36 +3,33 @@ const dotenv = require("dotenv");
 const app = require(`${__dirname}/app`);
 
 process.on("uncaughtException", (err) => {
-	console.log(err.name, err.message);
-	console.log("UNHANDLED EXCEPTION!!");
-	console.log("Shutting down...");
-	// server.close(() => {
-	process.exit(1);
-	// });
+  console.log(err.name, err.message);
+  console.log("UNHANDLED EXCEPTION!!");
+  console.log("Shutting down...");
+  process.exit(1);
 });
 
-// console.log(process.env);
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT;
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
 
 mongoose.connect(DB, {}).then(() => {
-	console.log("connected to database...");
+  console.log("connected to database...");
 });
-// .catch((err) => console.log("Error connecting to database..."));
 
 const server = app.listen(port, () => {
-	console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
 
 process.on("unhandledRejection", (err) => {
-	console.log(err.name, err.message);
-	console.log("UNHANDLED REJECTION!!");
-	console.log("Shutting down...");
-	server.close(() => {
-		process.exit(1);
-	});
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION!!");
+  console.log("Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
 });
-
-// console.log(x);
