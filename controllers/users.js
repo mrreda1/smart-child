a313898ec26b95ae5c8f2ded70f220f595e8f96e
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2026-04-07 08:41:25 +0200

    Adding factory methods for more generalization.

--- controllers/users.js
+++ controllers/users.js
@@ -1,103 +1,52 @@
 const User = require("./../models/user");
 const catchAsync = require("./../utils/catchAsync");
-const APIFeatures = require("./../utils/apifeatures");
 const AppError = require("./../utils/appError");
 const filterObj = require("./../utils/filterObj");
+const factory = require("./../controllers/handlerFactory");
 
-exports.getAllUsers = catchAsync(async (req, res) => {
-	const features = new APIFeatures(User.find(), req.query)
-		.sort()
-		.limit()
-		.filter()
-		.paginate();
+exports.getUser = factory.getOne(User);
 
-	const users = await features.query;
+exports.deleteUser = factory.deleteOne(User);
 
-	res.status(200).json({
-		status: "success",
-		results: users.length,
-		data: {
-			users,
-		},
-	});
-});
-
-exports.getUser = catchAsync(async (req, res, next) => {
-	const id = req.params.id;
-	const user = await User.findById(id);
-
-	if (!user) {
-		return next(new AppError("User not found!", 404));
-	}
-
-	res.status(200).json({
-		status: "success",
-		data: {
-			user,
-		},
-	});
-});
-
-exports.deleteUser = catchAsync(async (req, res, next) => {
-	const id = req.params.id;
-	const user = await User.findByIdAndDelete(id);
-
-  console.log(id, user);
-
-	if (!user) {
-		return next(new AppError("User not found!", 404));
-	}
-
-	res.status(204).json({
-		status: "success",
-		data: null,
-	});
-});
-
-exports.createUser = (req, res) => {
-	res.status(500).json({
-		status: "fail",
-		message: "This route is under construction",
-	});
-};
+exports.getAllUsers = factory.getMany(User);
 
 exports.updateUser = catchAsync(async (req, res, next) => {
-	if (req.body.password || req.body.passwordConfirm) {
-		return next(
-			new AppError("This route is not for user password updates.", 400),
-		);
-	}
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError("This route is not for user password updates.", 400),
+    );
+  }
 
-	const filteredBody = filterObj(req.body, "name", "email");
-	const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-		runValidators: true,
-		new: true,
-	});
+  const filteredBody = filterObj(req.body, "name", "email");
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    runValidators: true,
+    new: true,
+  });
 
-	res.status(200).json({
-		status: "success",
-		data: {
-			user: updatedUser,
-		},
-	});
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
 });
 
 exports.updateMyInfo = (req, res, next) => {
-	const user = req.user;
+  const user = req.user;
 
-	if (req.body.password || req.body.passwordConfirm) {
-		return next(new AppError("You cannot update password!", 400));
-	}
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError("You cannot update password!", 400));
+  }
 
-	res.status(200).json({
-		status: "success",
-	});
+  res.status(200).json({
+    status: "success",
+  });
 };
 
 exports.deleteMyAccount = catchAsync(async (req, res, next) => {
-	await User.findByIdAndUpdate(req.user.id, { active: false });
-	res.status(204).json({
-		status: "success",
-		data: null,
-	});
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
 });
