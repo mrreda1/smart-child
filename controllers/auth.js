ef44de5d883b33fea4e8df178211e605fc393a20
= <=>
2026-04-08 22:59:06 +0200

    Linked Reset Password Email With Client

--- controllers/auth.js
+++ controllers/auth.js
@@ -84,9 +84,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const resetToken = user.createPasswordResetToken(60);
   await user.save();
-  const resetURL = `${req.protocol}://${req.get(
-    'host',
-  )}/api/v1/users/resetPassword/${resetToken}`;
+  const resetURL = `${process.env.CLIENT_URL}/reset-password/${resetToken}`;
 
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
