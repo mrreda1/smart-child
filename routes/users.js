3a9b45fca710f766173b1532315e5bd5473f08a8
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2026-04-09 08:09:23 +0200

    quick fix: change login request type to post

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,37 @@
-const express = require("express");
+const express = require('express');
 const userController = require(`${__dirname}/../controllers/users`);
 const authController = require(`${__dirname}/../controllers/auth`);
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
-router.get("/login", authController.login);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
-router.post("/forgotPassword", authController.forgotPassword);
-router.patch("/resetPassword/:token", authController.resetPassword);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 router
-	.route("/updateMyPassword")
-	.patch(authController.protect, authController.updatePassword);
-router.route("/updateMyInfo").patch(authController.protect, userController.updateUser);
-router.route("/deleteMyAccount").delete(authController.protect, userController.deleteMyAccount);
+  .route('/updateMyPassword')
+  .patch(authController.protect, authController.updatePassword);
 router
-	.route("/delete/:id")
-	.delete(authController.protect, authController.restrictTo("admin"), userController.deleteUser);
+  .route('/updateMyInfo')
+  .patch(authController.protect, userController.updateUser);
+router
+  .route('/deleteMyAccount')
+  .delete(authController.protect, userController.deleteMyAccount);
+router
+  .route('/delete/:id')
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    userController.deleteUser,
+  );
+
+router.route('/').get(userController.getAllUsers);
 
 router
-	.route("/")
-	.get(userController.getAllUsers)
-
-router
-	.route("/:id")
-	.get(userController.getUser)
-	.patch(userController.updateUser)
-	.delete(userController.deleteUser);
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
